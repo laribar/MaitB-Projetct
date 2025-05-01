4238b9093d002ae06617a6c562b97f4a785a115c
Larissa Barbaroto <167935713+laribar@users.noreply.github.com>
2025-05-01 18:12:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,8 @@ def logout():
     session.pop('usuario', None)
     return redirect(url_for('login'))
 
+# ... (suas rotas e outras funções acima)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
