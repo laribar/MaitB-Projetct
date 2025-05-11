1e6d24d0e39f284d989c06ab40ec050c4993e7c5
Larissa Barbaroto <167935713+laribar@users.noreply.github.com>
2025-05-11 14:08:14 -0300

    Update forcar_simulacao.py

--- forcar_simulacao.py
+++ forcar_simulacao.py
@@ -9,7 +9,7 @@ end = start + timedelta(hours=5)
 
 df_candles = get_stock_data("BTC-USD", interval="1h", period="90d")
 df_candles.index = pd.to_datetime(df_candles.index)
-df_candles.index = df_candles.index.tz_localize("UTC").tz_convert(BR_TZ)
+df_candles.index = df_candles.index.tz_convert(BR_TZ)
 
 df_check = df_candles[(df_candles.index >= start) & (df_candles.index <= end)]
 print("\n🧪 Candles encontrados no intervalo desejado:")
